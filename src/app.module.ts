73faa48c7a9e7c7c924ee8d042b40fcc93f06eff
geanxcezar <geanxcezar@gmail.com>
2023-05-19 20:30:55 -0300

    commit ajuste de Paciente; Pscicologos

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './database/PrismaService';
 import { UsuariosModule } from './modules/usuarios/usuarios.module';
+import { PacientesModule } from './modules/pacientes/pacientes.module';
+import { PsicologosModule } from './modules/psicologos/psicologos.module';
 
 @Module({  
-  imports: [UsuariosModule],
+  imports: [UsuariosModule, PacientesModule, PsicologosModule],
   controllers: [AppController],
   providers: [AppService, PrismaService]
 })
